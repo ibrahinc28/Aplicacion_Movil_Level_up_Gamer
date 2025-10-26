9c85e1183031ae29559d7d6a217f1a3739cb7da8
Vania <Van.carvajal@duocuc.cl>
2025-10-26 14:54:44 -0300

    estado

--- app/src/main/java/com/example/levelupgamermovil/model/CarritoEstado.kt
+++ app/src/main/java/com/example/levelupgamermovil/model/CarritoEstado.kt
@@ -1,4 +1,11 @@
 package com.example.levelupgamermovil.model
 
-class CarritoEstado {
-}
+data class CarritoEstado (
+    val items: List<ItemCarrito> = emptyList(),
+    val subtotal: Double = 0.0,
+    val costoEnvio: Double = 5000.0,
+    val total: Double = 0.0,
+    val totalArticulos: Int = 0,
+    val mensajeConfirmado: String? = null,
+    val compraFinalizada: Boolean = false
+)
