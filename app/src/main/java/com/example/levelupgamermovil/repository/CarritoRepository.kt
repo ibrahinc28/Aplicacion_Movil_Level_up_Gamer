49c985fb3d2320b92212d1ab1957e31726b3c4fa
Vania <Van.carvajal@duocuc.cl>
2025-10-26 14:59:01 -0300

    repo

--- app/src/main/java/com/example/levelupgamermovil/repository/CarritoRepository.kt
+++ app/src/main/java/com/example/levelupgamermovil/repository/CarritoRepository.kt
@@ -1,4 +1,15 @@
 package com.example.levelupgamermovil.repository
 
-interface CarritoRepository {
+import com.example.carritoapp.model.CarritoDao
+import com.example.carritoapp.model.CarritoItemEntity
+import com.example.levelupgamermovil.model.CarritoItemEntity
+import kotlinx.coroutines.flow.Flow
+
+class CarritoRepository(private val dao: CarritoDao){
+    fun obtenerItemsCarrito(): Flow<List<CarritoItemEntity>> = dao.obtenerItemsCarrito()
+
+    suspend fun insertar(item: CarritoItemEntity) = dao.insertar(item)
+    suspend fun actualizar(item: CarritoItemEntity) = dao.actualizar(item)
+    suspend fun eliminarPorCodigo(codigo: String) = dao.eliminarPorCodigo(codigo)
+    suspend fun vaciar() = dao.vaciar()
 }
