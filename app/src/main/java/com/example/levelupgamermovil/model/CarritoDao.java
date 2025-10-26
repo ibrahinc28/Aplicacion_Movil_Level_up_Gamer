abbbeaf4ff8fdabfe4810463d422a9be4a679f35
Vania <Van.carvajal@duocuc.cl>
2025-10-26 14:54:01 -0300

    Dao

--- app/src/main/java/com/example/levelupgamermovil/model/CarritoDao.java
+++ app/src/main/java/com/example/levelupgamermovil/model/CarritoDao.java
@@ -1,4 +1,26 @@
 package com.example.levelupgamermovil.model;
 
-public class CarritoDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+@Dao
+interface CarritoDao {
+
+    @Query("SELECT * FROM carrito_items")
+    fun obtenerItemsCarrito(): Flow<List<CarritoItemEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertar(item: CarritoItemEntity)
+
+    @Update
+    suspend fun actualizar(item: CarritoItemEntity)
+
+    @Query("DELETE FROM carrito_items WHERE codigoProducto = :codigo")
+    suspend fun eliminarPorCodigo(codigo: String)
+
+    @Query("DELETE FROM carrito_items")
+    suspend fun vaciar()
 }
