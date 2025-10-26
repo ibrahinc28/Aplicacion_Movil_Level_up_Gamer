645ce0140d1069ec45d09315ac0d917cfc0544e8
Vania <Van.carvajal@duocuc.cl>
2025-10-25 21:17:34 -0300

    AppDataBase

--- app/src/main/java/com/example/carritoapp/model/AppDatabase.kt
+++ app/src/main/java/com/example/carritoapp/model/AppDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-
 @Database(entities = [CarritoItemEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase(){
     abstract fun carritoDao(): CarritoDao
