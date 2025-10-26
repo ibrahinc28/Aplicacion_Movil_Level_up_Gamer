7f87f55f4924db3ac946fc3b50842c6ae2461a58
Vania <Van.carvajal@duocuc.cl>
2025-10-26 12:41:28 -0300

    AppData

--- app/src/main/java/com/example/carritoapp/model/AppDatabase.kt
+++ app/src/main/java/com/example/carritoapp/model/AppDatabase.kt
@@ -4,6 +4,27 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+@Database(entities = [CarritoItemEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase(){
+    abstract fun carritoDao(): CarritoDao
+
+    companion object{
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDataBase(context: Context): AppDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "carrito_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
+
 @Database(entities = [Producto::class], version = 1)
 abstract class AppDatabase : RoomDatabase(){
     abstract fun productoDao(): ProductoDao
