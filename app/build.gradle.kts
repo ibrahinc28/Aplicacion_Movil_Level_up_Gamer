dd7312d0d15a2fb0ed04fedbeea20a219841259b
cesar <cesarmaldonadomecanicodae13@gmail.com>
2025-10-26 17:24:21 -0300

    resolviendo problemas de dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("org.jetbrains.kotlin.kapt")
+
 }
 
 android {
@@ -42,11 +42,7 @@ android {
 }
 
 dependencies {
-
-    implementation ("androidx.room:room-runtime:2.6.1")
-    kapt ("androidx.room:room-compiler:2.6.1")
-    implementation ("androidx.room:room-ktx:2.6.1")
-
+    
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation("androidx.room:room-runtime:2.6.1")
