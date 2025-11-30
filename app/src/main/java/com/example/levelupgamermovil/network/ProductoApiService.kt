29810f36d0c8b4dd0415a9233e258533c4f69cbe
Gino Fierro <gin.fierro@duocuc.cl>
2025-11-30 20:25:25 -0300

    Error arreglado, API de productos usaba link incorrecto.

--- app/src/main/java/com/example/levelupgamermovil/network/ProductoApiService.kt
+++ app/src/main/java/com/example/levelupgamermovil/network/ProductoApiService.kt
@@ -1,7 +1,6 @@
 package com.example.levelupgamermovil.network
 
 import com.example.levelupgamermovil.model.Producto
-import com.example.levelupgamermovil.model.UsuarioAPI
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
